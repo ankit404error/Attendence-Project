0e0c6c02f585366fe974829910ed1c6b456d48ef
Ankit <pandeyankit54562@gmail.com>
2024-04-24 13:32:36 +0530

    updated new function to tell total bunk possible

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function updateAttendance() {
     document.getElementById("updatedAttendance").textContent = `Your updated attendance is: ${updatedAttendance.toFixed(2)}%`;
 }
 
+function calculateBunkClasses() {
+    const attendedClasses = parseInt(document.getElementById("attendedClasses").value);
+    const deliveredClasses = parseInt(document.getElementById("deliveredClasses").value);
+    const currentAttendance = (attendedClasses * 100) / deliveredClasses;
+
+    let bunkClasses = 0;
+    while (bunkClasses<attendedClasses) {
+        const updatedAttendance = ((attendedClasses) * 100) / (deliveredClasses + bunkClasses);
+        if (updatedAttendance <= 76) {
+            break;
+        }
+        bunkClasses++;
+    }
+    document.getElementById("bunkP").textContent = `Total bunk classes possible: ${bunkClasses}`;
+}
